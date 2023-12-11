25071d97bb7ebf96b67e0325df00a838c951a19d
rkpattnaik780 <rkpattnaik780@gmail.com>
2023-12-11 19:13:26 +0530

    fix: missing url param

--- ci/security-scan/quay_security_analysis.py
+++ ci/security-scan/quay_security_analysis.py
@@ -53,7 +53,7 @@ def get_image_security_reports(commit_id_path, IMAGES):
             "Authorization": "Bearer 3PZX0UYX6FSENKQ14I1VTHUJ4KGBS8L5LHJ0W1RN7TPHFVQ4P0NR7VQNCZIFRC9B_1"
         }
 
-        url = f"https://quay.io/api/v1/repository/opendatahub/workbench-images/manifest/{digest}/security"
+        url = f"https://quay.io/api/v1/repository/opendatahub/workbench-images/manifest/{digest}/security?vulnerabilities=true"
 
         response = requests.get(url, headers=headers)
         data = response.json()
